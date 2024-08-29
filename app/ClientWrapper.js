5ca37b9ed9075248ddb76c0e607dc17c183e3f59
Itai Turbahn <itai@dynamic.xyz>
2024-08-29 07:04:04 -0700

    fix environment id

--- app/ClientWrapper.js
+++ app/ClientWrapper.js
@@ -5,65 +5,10 @@ import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 export default function ClientWrapper({ children }) {
   const { user, setShowAuthFlow } = useDynamicContext();
 
-  const customCSS = `
-    @keyframes slideInFromRight {
-      from {
-        transform: translateX(100%);
-      }
-      to {
-        transform: translateX(0);
-      }
-    }
-
-    @media (min-width: 768px) {
-      .accordion-item {
-        max-height: 100vh !important;
-      }
-
-      .modal, .dynamic-widget-modal, .dynamic-widget-card {
-        right: 0 !important;
-        top: 0 !important;
-        transform: none !important;
-        height: 100vh !important;
-        border-radius: 0 !important;
-        left: auto !important;
-      }
-
-      .modal-card {
-        border-radius: 0 !important;
-      }
-
-      .social-redirect-view__container, .wallet-no-access__container, .pending-signature__container {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-      }
-
-      .footer-options-switcher__container {
-        border-radius: 0 !important;
-        position: absolute !important;
-        bottom: 0 !important;
-        left: 0 !important;
-        right: 0 !important;
-      }
-
-      .dynamic-footer, .tos-and-pp__footer {
-        position: fixed !important;
-        bottom: 0 !important;
-        left: 0 !important;
-        right: 0 !important;
-      }
-
-      .tos-and-pp__footer {
-        bottom: 30px !important;
-      }
-    }
-  `;
+  
 
   return (
     <>
-      <style>{customCSS}</style>
       <div className="flex flex-col items-center justify-center min-h-screen bg-blue-50">
         <img
           src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/%5BWordmark-primary%5D%20(2)-jSotf5LNO2iZLyHlkThBAGhwR2fVCb.svg"
--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,63 @@ import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import ClientWrapper from './ClientWrapper';
 
 export default function RootLayout({ children }) {
+  const customCSS = `
+  @keyframes slideInFromRight {
+    from {
+      transform: translateX(100%);
+    }
+    to {
+      transform: translateX(0);
+    }
+  }
+
+  @media (min-width: 768px) {
+    .accordion-item {
+      max-height: 100vh !important;
+    }
+
+    .modal, .dynamic-widget-modal, .dynamic-widget-card {
+      right: 0 !important;
+      top: 0 !important;
+      transform: none !important;
+      height: 100vh !important;
+      border-radius: 0 !important;
+      left: auto !important;
+    }
+
+    .modal-card {
+      border-radius: 0 !important;
+    }
+
+    .social-redirect-view__container, .wallet-no-access__container, .pending-signature__container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+    }
+
+    .footer-options-switcher__container {
+      border-radius: 0 !important;
+      position: absolute !important;
+      bottom: 0 !important;
+      left: 0 !important;
+      right: 0 !important;
+    }
+
+    .dynamic-footer, .tos-and-pp__footer {
+      position: fixed !important;
+      bottom: 0 !important;
+      left: 0 !important;
+      right: 0 !important;
+    }
+
+    .tos-and-pp__footer {
+      bottom: 30px !important;
+    }
+  }
+`;
+
+
   return (
     <html lang="en">
       <body>
@@ -11,6 +68,7 @@ export default function RootLayout({ children }) {
           settings={{
             environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID,
             walletConnectors: [EthereumWalletConnectors],
+            cssOverrides: customCSS,
           }}
         >
           <ClientWrapper>{children}</ClientWrapper>
